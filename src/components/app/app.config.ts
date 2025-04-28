e18af05ba02f76acfc06bdc36c3edbcd3e181111
Saltman0 <baudoin.mathieu@protonmail.com>
2025-04-29 00:31:28 +0200

    :sparkles: Config updated

--- src/components/app/app.config.ts
+++ src/components/app/app.config.ts
@@ -1,8 +1,39 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
+import { ApplicationConfig, inject } from "@angular/core";
+import { provideRouter } from "@angular/router";
+import { routes } from "./app.routes";
+import { provideHttpClient, withFetch } from "@angular/common/http";
+import { provideApollo } from "apollo-angular";
+import { HttpLink } from "apollo-angular/http";
+import { ApolloLink, InMemoryCache } from "@apollo/client/core";
+import { setContext } from "@apollo/client/link/context";
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(
+      withFetch()
+    ),
+    provideApollo(() => {
+      const httpLink = inject(HttpLink);
+
+      const auth = setContext(() => {
+        const token = localStorage.getItem("jwtToken");
+
+        if (token === null) {
+          return {};
+        } else {
+          return {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          };
+        }
+      });
+
+      return {
+        link: ApolloLink.from([auth, httpLink.create({ uri: 'http://localhost:4000/' })]),
+        cache: new InMemoryCache()
+      };
+    })
+  ],
 };
