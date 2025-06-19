e8d78a61e54a0336dc086b97cacb60d1cfbaeafb
Saltman0 <baudoin.mathieu@protonmail.com>
2025-06-20 00:19:24 +0200

    :fire: Booking model updated

--- src/models/booking.model.ts
+++ src/models/booking.model.ts
@@ -3,13 +3,11 @@ import { BookingSeatModel } from "./bookingSeat.model";
 
 export class BookingModel {
     id: number;
-    qrCode: string;
     showtime: ShowtimeModel;
     bookingSeats: BookingSeatModel[];
 
-    constructor(id: number, qrCode: string, showtime: ShowtimeModel, bookingSeats: BookingSeatModel[]) {
+    constructor(id: number, showtime: ShowtimeModel, bookingSeats: BookingSeatModel[]) {
         this.id = id;
-        this.qrCode = qrCode;
         this.showtime = showtime;
         this.bookingSeats = bookingSeats;
     }
