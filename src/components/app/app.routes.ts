36c3cedd3eb99fcc6a5b4d265f29c2b8cc7a9303
Saltman0 <baudoin.mathieu@protonmail.com>
2025-06-21 08:45:07 +0200

    :sparkles: Routes : employee component added

--- src/components/app/app.routes.ts
+++ src/components/app/app.routes.ts
@@ -1,11 +1,13 @@
 import {Routes} from '@angular/router';
 import {HomeComponent} from '../home/home.component';
 import {OrderComponent} from '../order/order.component';
+import {EmployeeComponent} from '../employee/employee.component';
 
 export const root: string = "http://localhost:4200/";
 
 export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', title: "Home page", component: HomeComponent },
-  { path: 'order', title: "Order page", component: OrderComponent }
+  { path: 'order', title: "Order page", component: OrderComponent },
+  { path: 'employee', title: "Employee page", component: EmployeeComponent }
 ];
