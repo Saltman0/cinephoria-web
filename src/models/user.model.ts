dddd290c54bc2cbca52818bf1ea29434d56c9beb
Saltman0 <baudoin.mathieu@protonmail.com>
2025-06-21 08:40:50 +0200

    :sparkles: User model updated

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,11 +1,15 @@
 export class UserModel {
     id: number;
+    email: string;
     firstName: string;
     lastName: string;
+    phoneNumber: string;
 
-    constructor(id: number, firstName: string, lastName: string) {
+    constructor(id: number, email: string, firstName: string, lastName: string, phoneNumber: string) {
         this.id = id;
+        this.email = email;
         this.firstName = firstName;
         this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
     }
 }
