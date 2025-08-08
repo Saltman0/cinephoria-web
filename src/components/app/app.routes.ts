33d84678ddde8bd9583b512257d7e4ab2a510e82
Saltman0 <baudoin.mathieu@protonmail.com>
2025-08-08 13:51:30 +0200

    :sparkles: App routes : movie-showtime-settings route added

--- src/components/app/app.routes.ts
+++ src/components/app/app.routes.ts
@@ -2,6 +2,7 @@ import {Routes} from '@angular/router';
 import {HomeComponent} from '../home/home.component';
 import {OrderComponent} from '../order/order.component';
 import {EmployeeComponent} from '../employee/employee.component';
+import {MovieShowtimeSettingsComponent} from '../movie-showtime-settings/movie-showtime-settings.component';
 
 export const root: string = "http://localhost:4200/";
 
@@ -9,5 +10,6 @@ export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', title: "Home page", component: HomeComponent },
   { path: 'order', title: "Order page", component: OrderComponent },
-  { path: 'employee', title: "Employee page", component: EmployeeComponent }
+  { path: 'employee', title: "Employee page", component: EmployeeComponent },
+  { path: 'movie-showtime-settings', title: "Movie Showtime settings page", component: MovieShowtimeSettingsComponent }
 ];
