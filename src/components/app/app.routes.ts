24a111ceeabac45285c409958f6ca0d5bff5fd23
Saltman0 <baudoin.mathieu@protonmail.com>
2025-08-10 17:13:45 +0200

    :sparkles: App routes : hall-settings route added

--- src/components/app/app.routes.ts
+++ src/components/app/app.routes.ts
@@ -3,6 +3,7 @@ import {HomeComponent} from '../home/home.component';
 import {OrderComponent} from '../order/order.component';
 import {EmployeeComponent} from '../employee/employee.component';
 import {MovieShowtimeSettingsComponent} from '../movie-showtime-settings/movie-showtime-settings.component';
+import {HallSettingsComponent} from '../hall-settings/hall-settings.component';
 
 export const root: string = "http://localhost:4200/";
 
@@ -11,5 +12,6 @@ export const routes: Routes = [
   { path: 'home', title: "Home page", component: HomeComponent },
   { path: 'order', title: "Order page", component: OrderComponent },
   { path: 'employee', title: "Employee page", component: EmployeeComponent },
-  { path: 'movie-showtime-settings', title: "Movie Showtime settings page", component: MovieShowtimeSettingsComponent }
+  { path: 'movie-showtime-settings', title: "Movie Showtime settings page", component: MovieShowtimeSettingsComponent },
+  { path: 'hall-settings', title: "Hall settings page", component: HallSettingsComponent }
 ];
