3e52f167124349767a7e5f1f4a19d983c8d5e56c
Saltman0 <baudoin.mathieu@protonmail.com>
2025-08-22 13:38:27 +0200

    :sparkles: Account creation component added

---
+++ src/components/account-creation/account-creation.component.ts
@@ -0,0 +1,37 @@
+import {Component} from '@angular/core';
+import {HeaderComponent} from '../header/header.component';
+import {FooterComponent} from '../footer/footer.component';
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {ApiService} from '../../services/api/api.service';
+
+@Component({
+  selector: 'app-account-creation',
+  imports: [
+    HeaderComponent,
+    FooterComponent,
+    ReactiveFormsModule
+  ],
+  templateUrl: './account-creation.component.html',
+  styleUrl: './account-creation.component.scss'
+})
+export class AccountCreationComponent {
+  accountCreationForm = new FormGroup({
+    email: new FormControl("", [Validators.required, Validators.email]),
+    password: new FormControl("", [Validators.required]),
+    firstName: new FormControl("", [Validators.required]),
+    lastName: new FormControl("", [Validators.required]),
+    phoneNumber: new FormControl("", [Validators.required, Validators.pattern("^\\+?[0-9]{1,3}([-\\s]?[0-9]{1,4}){2,4}$\n")])
+  });
+
+  constructor(private readonly apiService: ApiService) {}
+
+  async submit() {
+    await this.apiService.createUser(
+      <string> this.accountCreationForm.value.email,
+      <string> this.accountCreationForm.value.password,
+      <string> this.accountCreationForm.value.firstName,
+      <string> this.accountCreationForm.value.lastName,
+      <string> this.accountCreationForm.value.phoneNumber
+    );
+  }
+}
