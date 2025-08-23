b6261581162c019644d2d001ade435a33e340e23
Saltman0 <baudoin.mathieu@protonmail.com>
2025-08-23 21:49:09 +0200

    :sparkles: Administrator component added

--- src/components/administrator/administrator.component.ts
+++ src/components/administrator/administrator.component.ts
@@ -1,29 +1,20 @@
 import {Component} from '@angular/core';
 import {HeaderComponent} from '../header/header.component';
 import {FooterComponent} from '../footer/footer.component';
-import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
-import {ApiService} from '../../services/api/api.service';
 import {Router} from '@angular/router';
+import {NgOptimizedImage} from '@angular/common';
 
 @Component({
   selector: 'app-account-creation',
   imports: [
     HeaderComponent,
     FooterComponent,
-    ReactiveFormsModule
+    NgOptimizedImage
   ],
   templateUrl: './administrator.component.html',
   styleUrl: './administrator.component.scss'
 })
 export class AdministratorComponent {
-  accountCreationForm = new FormGroup({
-    email: new FormControl("", [Validators.required, Validators.email]),
-    password: new FormControl("", [Validators.required]),
-    firstName: new FormControl("", [Validators.required]),
-    lastName: new FormControl("", [Validators.required]),
-    phoneNumber: new FormControl("", [Validators.required, Validators.pattern("^\\+?[0-9]{1,3}([-\\s]?[0-9]{1,4}){2,4}$\n")])
-  });
-
   constructor(private readonly router: Router) {}
 
   redirectToMovieShowtimeSettingsPage() {
