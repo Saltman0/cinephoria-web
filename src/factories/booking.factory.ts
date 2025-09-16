076488a411073988b031aaff5afdc5b1edf34ad4
Saltman0 <baudoin.mathieu@protonmail.com>
2025-09-17 00:45:46 +0200

    ✨ Booking factory updated

--- src/factories/booking.factory.ts
+++ src/factories/booking.factory.ts
@@ -1,15 +1,16 @@
-import { Injectable } from "@angular/core";
-import { ShowtimeModel } from "../models/showtime.model";
-import { BookingModel } from "../models/booking.model";
-import { BookingSeatModel } from "../models/bookingSeat.model";
+import {Injectable} from "@angular/core";
+import {ShowtimeModel} from "../models/showtime.model";
+import {BookingModel} from "../models/booking.model";
+import {BookingSeatModel} from "../models/bookingSeat.model";
+import {UserModel} from '../models/user.model';
 
 @Injectable({
     providedIn: 'root'
 })
 export class BookingFactory {
 
-    public create(id: number, showtime: ShowtimeModel, bookingSeats: BookingSeatModel[]): BookingModel {
-        return new BookingModel(id, showtime, bookingSeats);
+    public create(id: number, qrCode: string, user: UserModel, showtime: ShowtimeModel, bookingSeats: BookingSeatModel[]): BookingModel {
+        return new BookingModel(id, qrCode, user, showtime, bookingSeats);
     }
 
 }
