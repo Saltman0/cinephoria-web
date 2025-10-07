33a55953bbf97904acfa149c337b12dce9fef4aa
Saltman0 <baudoin.mathieu@protonmail.com>
2025-10-07 22:47:31 +0200

    🎨 Header component updated

--- src/components/header/header.component.ts
+++ src/components/header/header.component.ts
@@ -1,9 +1,6 @@
-import { Component } from '@angular/core';
-import { NgOptimizedImage } from "@angular/common";
+import {Component} from '@angular/core';
+import {NgOptimizedImage} from "@angular/common";
 import {RouterLink, RouterLinkActive} from '@angular/router';
-/*import { Router } from "@angular/router";
-import { DatabaseService } from "../../services/database/database.service";
-import { LocalStorageService } from "../../services/local-storage/local-storage.service";*/
 
 @Component({
   selector: 'app-header',
@@ -17,18 +14,4 @@ import { LocalStorageService } from "../../services/local-storage/local-storage.
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
-export class HeaderComponent {
-  firstName: string = "Mathieu";
-  lastName: string = "Baudoin";
-
-  /*constructor(private readonly router: Router,
-              private readonly databaseService: DatabaseService,
-              private readonly localStorageService: LocalStorageService) {}*/
-
-  /*async disconnect(): Promise<void> {
-    this.databaseService.closeDatabase();
-    this.databaseService.deleteDatabase();
-    this.localStorageService.deleteJwtToken();
-    await this.router.navigate(['login']);
-  }*/
-}
+export class HeaderComponent {}
