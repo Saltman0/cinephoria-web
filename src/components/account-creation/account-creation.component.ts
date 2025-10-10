5a7446ab61a053f3a6d37cee91d35146349e70bc
Saltman0 <baudoin.mathieu@protonmail.com>
2025-10-11 01:03:36 +0200

    💄 Account creation component : HTML updated

--- src/components/account-creation/account-creation.component.ts
+++ src/components/account-creation/account-creation.component.ts
@@ -3,13 +3,17 @@ import {HeaderComponent} from '../header/header.component';
 import {FooterComponent} from '../footer/footer.component';
 import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
 import {ApiService} from '../../services/api/api.service';
+import {NavMobileComponent} from '../nav-mobile/nav-mobile.component';
+import {NgOptimizedImage} from '@angular/common';
 
 @Component({
   selector: 'app-account-creation',
   imports: [
     HeaderComponent,
     FooterComponent,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    NavMobileComponent,
+    NgOptimizedImage
   ],
   templateUrl: './account-creation.component.html',
   styleUrl: './account-creation.component.scss'
@@ -20,7 +24,10 @@ export class AccountCreationComponent {
     password: new FormControl("", [Validators.required]),
     firstName: new FormControl("", [Validators.required]),
     lastName: new FormControl("", [Validators.required]),
-    phoneNumber: new FormControl("", [Validators.required, Validators.pattern("^\\+?[0-9]{1,3}([-\\s]?[0-9]{1,4}){2,4}$\n")])
+    phoneNumber: new FormControl(
+      "",
+      [Validators.required, Validators.pattern("^\\+?[0-9]{1,3}([-\\s]?[0-9]{1,4}){2,4}$\n")]
+    )
   });
 
   constructor(private readonly apiService: ApiService) {}
