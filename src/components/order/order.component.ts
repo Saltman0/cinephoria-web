1c30fd89ecd050c39d76810e6d60de17906bf9f5
Saltman0 <baudoin.mathieu@protonmail.com>
2025-10-12 01:09:08 +0200

    💄 Order component updated

--- src/components/order/order.component.ts
+++ src/components/order/order.component.ts
@@ -6,6 +6,8 @@ import {NgOptimizedImage} from '@angular/common';
 import {BookingModel} from '../../models/booking.model';
 import {ReactiveFormsModule} from '@angular/forms';
 import {BookingRenderer} from '../../renderers/booking.renderer';
+import {NavMobileComponent} from '../nav-mobile/nav-mobile.component';
+import {CinemaModel} from '../../models/cinema.model';
 
 @Component({
   selector: 'app-home',
@@ -14,12 +16,15 @@ import {BookingRenderer} from '../../renderers/booking.renderer';
     FooterComponent,
     NgOptimizedImage,
     NgOptimizedImage,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    NavMobileComponent
   ],
   templateUrl: './order.component.html',
   styleUrl: './order.component.scss'
 })
 export class OrderComponent {
+  cinemaList: { id: number; name: string; }[] = [];
+
   bookingList: {
     id: number,
     movieTitle: string,
@@ -34,10 +39,29 @@ export class OrderComponent {
     private readonly apiService: ApiService) {}
 
   async ngOnInit(): Promise<void> {
+    await this.loadCinemas();
+  }
+
+  public async loadCinemas(): Promise<void> {
+    const cinemas: CinemaModel[] = await this.apiService.getCinemas();
+
+    this.cinemaList = [];
+    for (const cinema of cinemas) {
+      this.cinemaList.push(this.bookingRenderer.renderCinema(cinema));
+    }
+  }
+
+  public async loadBookings(): Promise<void> {
+    this.resetBookings();
+
     const bookings: BookingModel[] = await this.apiService.getBookings(1);
 
     for (const booking of bookings) {
       this.bookingList.push(await this.bookingRenderer.render(booking));
     }
+  }
+
+  private resetBookings(): void {
+    this.bookingList = [];
   }
 }
