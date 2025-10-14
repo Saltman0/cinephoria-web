b17f2d5b44c00831c808789b242d83677b27026b
Saltman0 <baudoin.mathieu@protonmail.com>
2025-10-14 23:19:15 +0200

    ✨ Add hall dialog component

---
+++ src/components/add-hall-dialog/add-hall-dialog.component.ts
@@ -0,0 +1,120 @@
+import {Component, ElementRef, output, ViewChild} from '@angular/core';
+import {LocalStorageService} from '../../services/local-storage/local-storage.service';
+import {ApiService} from '../../services/api/api.service';
+import {FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
+import {NgOptimizedImage} from '@angular/common';
+import {CinemaModel} from '../../models/cinema.model';
+import {HallSettingsRenderer} from '../../renderers/hall-settings.renderer';
+
+@Component({
+  selector: 'app-add-hall-dialog',
+  imports: [
+    FormsModule,
+    NgOptimizedImage,
+    ReactiveFormsModule
+  ],
+  templateUrl: './add-hall-dialog.component.html',
+  styleUrl: './add-hall-dialog.component.scss'
+})
+export class AddHallDialogComponent {
+  cinemaList: { id: number; name: string; }[] = [];
+
+  hallForm = new FormGroup({
+    cinemaId: new FormControl(
+      "", [Validators.required]
+    ),
+    number: new FormControl(
+      "", [Validators.required, Validators.min(1), Validators.max(50)]
+    ),
+    projectionQuality: new FormControl(
+      "", [Validators.required, Validators.minLength(1), Validators.maxLength(20)]
+    ),
+    nbRows: new FormControl(
+      "", [Validators.required, Validators.min(1), Validators.max(50)]
+    ),
+    nbSeats: new FormControl(
+      "", [Validators.required, Validators.min(1), Validators.max(100)]
+    )
+  });
+
+  isAddingHall: boolean = false;
+
+  readonly hallAddedEvent = output<boolean>();
+
+  @ViewChild('addHallDialog') addHallDialog!: ElementRef<HTMLDialogElement>;
+
+  constructor(
+    private readonly localStorageService: LocalStorageService,
+    private readonly apiService: ApiService,
+    private readonly hallSettingsRenderer: HallSettingsRenderer) {}
+
+  async ngOnInit(): Promise<void> {
+    await this.loadCinemas();
+  }
+
+  public async loadCinemas(): Promise<void> {
+    this.resetCinemas();
+
+    const cinemas: CinemaModel[] = await this.apiService.getCinemas();
+
+    for (const cinema of cinemas) {
+      this.cinemaList.push(this.hallSettingsRenderer.renderCinema(cinema));
+    }
+  }
+
+  public async createHall(): Promise<void> {
+    this.isAddingHall = true;
+
+    const jwtToken = await this.apiService.login(
+      "baudoin.mathieu@protonmail.com", "0123456789"
+    );
+
+    this.localStorageService.addJwtToken(jwtToken.value);
+
+    const hall = await this.apiService.createHall(
+      this.localStorageService.getJwtToken(),
+      <number><unknown> this.hallForm.value.number,
+      <string> this.hallForm.value.projectionQuality,
+      <number><unknown> this.hallForm.value.cinemaId
+    );
+
+    const nbRows = <number><unknown> this.hallForm.value.nbRows;
+    const nbSeats = <number><unknown> this.hallForm.value.nbSeats;
+    for (let i: number = 0; i < nbRows; i++) {
+
+      const row: string = String.fromCharCode(65 + i);
+      console.log('Row : ', row);
+
+      for (let j: number = 0; j < nbSeats; j++) {
+        const number: number = j + 1;
+        console.log('Number : ', number);
+
+        await this.apiService.createSeat(
+          this.localStorageService.getJwtToken(),
+          row,
+          number,
+          hall.id
+        );
+      }
+
+    }
+
+    this.hallAddedEvent.emit(true);
+
+    this.closeAddHallDialog();
+
+    this.isAddingHall = false;
+  }
+
+  public showAddHallDialog(): void {
+    this.addHallDialog.nativeElement.showModal();
+  }
+
+  public closeAddHallDialog(): void {
+    this.addHallDialog.nativeElement.close();
+  }
+
+  private resetCinemas(): void {
+    this.cinemaList = [];
+  }
+}
