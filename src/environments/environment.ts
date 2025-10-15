6299669e1d98e14180e49ae49fe3df8c65bab621
Saltman0 <baudoin.mathieu@protonmail.com>
2025-10-16 01:08:08 +0200

    ✨ SHOWTIME_API_URL environment variable added

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,6 +8,7 @@ export const environment = {
   GRAPHQL_API_URL: "http://172.18.0.5/",
   INFRASTRUCTURE_API_URL: "http://172.18.0.5/",
   MOVIE_API_URL: "http://172.18.0.5/",
+  SHOWTIME_API_URL: "http://172.18.0.5/",
   USER_API_URL: "http://172.18.0.5/"
 };
 
