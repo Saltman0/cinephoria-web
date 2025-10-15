23cf63b95f2638f707e2935d6cc023bd7722859b
Saltman0 <baudoin.mathieu@protonmail.com>
2025-10-15 13:54:41 +0200

    ✨ Forgotten password component updated

--- src/components/forgotten-password/forgotten-password.component.ts
+++ src/components/forgotten-password/forgotten-password.component.ts
@@ -3,29 +3,37 @@ import {HeaderComponent} from '../header/header.component';
 import {FooterComponent} from '../footer/footer.component';
 import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
 import {ApiService} from '../../services/api/api.service';
+import {NavMobileComponent} from '../nav-mobile/nav-mobile.component';
+import {NgOptimizedImage} from '@angular/common';
 
 @Component({
   selector: 'app-contact',
   imports: [
     HeaderComponent,
     FooterComponent,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    NavMobileComponent,
+    NgOptimizedImage
   ],
   templateUrl: './forgotten-password.component.html',
   styleUrl: './forgotten-password.component.scss'
 })
 export class ForgottenPasswordComponent {
   forgottenPasswordForm = new FormGroup({
-    email: new FormControl("", [Validators.required, Validators.email])
+    email: new FormControl(
+      "",
+      [Validators.required, Validators.email, Validators.minLength(5), Validators.maxLength(50)]
+    ),
   });
 
   constructor(private readonly apiService: ApiService) {}
 
-  async submit() {
+  public async submit() {
     await this.apiService.sendMail(
       <string> this.forgottenPasswordForm.value.email,
       "Cinéphoria - Réinitialisation de mot de passe",
-      "Votre mot de passe a été réinitialisé ! Veuillez modifier votre mot de passe vers le lien suivante : lien"
+      "Votre mot de passe a été réinitialisé ! " +
+      "Veuillez modifier votre mot de passe vers le lien suivant : lien"
     );
   }
 }
