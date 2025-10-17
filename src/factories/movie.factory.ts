25ceb7ed821edc40765eee478fd3a7d9c191a5a6
Saltman0 <baudoin.mathieu@protonmail.com>
2025-10-18 00:10:37 +0200

    ✨ Movie factory updated

--- src/factories/movie.factory.ts
+++ src/factories/movie.factory.ts
@@ -1,14 +1,23 @@
 import {Injectable} from "@angular/core";
 import {MovieModel} from "../models/movie.model";
 import {ShowtimeModel} from "../models/showtime.model";
+import {CategoryModel} from '../models/category.model';
 
 @Injectable({
     providedIn: 'root'
 })
 export class MovieFactory {
 
-    public create(id: number, favorite: boolean, title: string, imageUrl: string, minimumAge: number, showtimes: ShowtimeModel[]): MovieModel {
-        return new MovieModel(id, favorite, title, imageUrl, minimumAge, showtimes);
+    public create(
+      id: number,
+      favorite: boolean,
+      title: string,
+      imageUrl: string,
+      minimumAge: number,
+      showtimes: ShowtimeModel[],
+      category: CategoryModel
+    ): MovieModel {
+        return new MovieModel(id, favorite, title, imageUrl, minimumAge, showtimes, category);
     }
 
 }
