75a0d3e5979d98aceb598e3e9d2cfb1c64295018
Saltman0 <baudoin.mathieu@protonmail.com>
2025-10-19 18:22:57 +0200

    ✨ Login component added

---
+++ src/components/login/login.component.ts
@@ -0,0 +1,46 @@
+import {Component} from '@angular/core';
+import {HeaderComponent} from '../header/header.component';
+import {FooterComponent} from '../footer/footer.component';
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {ApiService} from '../../services/api/api.service';
+import {NavMobileComponent} from '../nav-mobile/nav-mobile.component';
+import {NgOptimizedImage} from '@angular/common';
+import {LocalStorageService} from '../../services/local-storage/local-storage.service';
+import {Router, RouterLink} from "@angular/router";
+
+@Component({
+  selector: 'app-login',
+  imports: [
+    HeaderComponent,
+    FooterComponent,
+    ReactiveFormsModule,
+    NavMobileComponent,
+    NgOptimizedImage,
+    RouterLink
+  ],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent {
+  loginForm = new FormGroup({
+    email: new FormControl("", [Validators.required, Validators.email]),
+    password: new FormControl("", [Validators.required])
+  });
+
+  constructor(
+      private readonly localStorageService: LocalStorageService,
+      private readonly apiService: ApiService,
+      private readonly router: Router
+  ) {}
+
+  public async connect(): Promise<void> {
+    const result = await this.apiService.login(
+        <string> this.loginForm.value.email,
+        <string> this.loginForm.value.password
+    );
+
+    this.localStorageService.addJwtToken(result.value);
+
+    await this.router.navigate(['/home']);
+  }
+}
