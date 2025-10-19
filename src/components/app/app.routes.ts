d301efc2a5383cf239e2fa2799c60f4066fc3bd6
Saltman0 <baudoin.mathieu@protonmail.com>
2025-10-19 18:21:50 +0200

    ✨ Routes updated

--- src/components/app/app.routes.ts
+++ src/components/app/app.routes.ts
@@ -1,5 +1,6 @@
 import {Routes} from '@angular/router';
 import {HomeComponent} from '../home/home.component';
+import {LoginComponent} from "../login/login.component";
 import {OrderComponent} from '../order/order.component';
 import {EmployeeListComponent} from '../employee-list/employee-list.component';
 import {MovieShowtimeSettingsComponent} from '../movie-showtime-settings/movie-showtime-settings.component';
@@ -15,6 +16,7 @@ export const root: string = "http://localhost:4200/";
 export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', title: "Home page", component: HomeComponent },
+  { path: 'login', title: "Login page", component: LoginComponent },
   { path: 'booking', title: "Booking page", component: BookingComponent },
   { path: 'order', title: "Order page", component: OrderComponent },
   { path: 'employee', title: "Employee page", component: EmployeeListComponent },
