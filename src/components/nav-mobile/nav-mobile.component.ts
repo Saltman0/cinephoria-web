eedeccfb5db8fbe27c4d849c7a1efb6338d36bd2
Saltman0 <baudoin.mathieu@protonmail.com>
2025-10-19 22:19:13 +0200

    ✨ Nav mobile component updated

--- src/components/nav-mobile/nav-mobile.component.ts
+++ src/components/nav-mobile/nav-mobile.component.ts
@@ -1,6 +1,9 @@
-import {Component} from '@angular/core';
-import {RouterLink, RouterLinkActive} from '@angular/router';
+import {Component, Input} from '@angular/core';
+import {Router, RouterLink, RouterLinkActive} from '@angular/router';
 import {NgOptimizedImage} from '@angular/common';
+import {LocalStorageService} from "../../services/local-storage/local-storage.service";
+import {ApiService} from "../../services/api/api.service";
+import {UserModel} from "../../models/user.model";
 
 @Component({
   selector: 'app-nav-mobile',
@@ -13,4 +16,27 @@ import {NgOptimizedImage} from '@angular/common';
   templateUrl: './nav-mobile.component.html',
   styleUrl: './nav-mobile.component.scss'
 })
-export class NavMobileComponent {}
+export class NavMobileComponent {
+  @Input() role: string = "visitor";
+
+  constructor(
+      private readonly localStorageService: LocalStorageService,
+      private readonly apiService: ApiService,
+      private readonly router: Router
+  ) {}
+
+  async ngOnInit(): Promise<void> {
+    const jwtToken: string|null = this.localStorageService.getJwtToken();
+
+    if (jwtToken !== null) {
+      const user: UserModel = await this.apiService.getUser(jwtToken);
+      this.role = user.role;
+    }
+  }
+
+  public async logout(): Promise<void> {
+    this.localStorageService.deleteJwtToken();
+
+    await this.router.navigate(['/home']);
+  }
+}
