6af61c3938cf1916f69349aeea04cd75d58fbfd3
Saltman0 <baudoin.mathieu@protonmail.com>
2025-10-19 18:22:17 +0200

    ✨ Header component updated

--- src/components/header/header.component.ts
+++ src/components/header/header.component.ts
@@ -1,6 +1,9 @@
 import {Component, Input} from '@angular/core';
 import {NgOptimizedImage} from "@angular/common";
-import {RouterLink, RouterLinkActive} from '@angular/router';
+import {Router, RouterLink, RouterLinkActive} from '@angular/router';
+import {LocalStorageService} from "../../services/local-storage/local-storage.service";
+import {ApiService} from "../../services/api/api.service";
+import {UserModel} from "../../models/user.model";
 
 @Component({
   selector: 'app-header',
@@ -15,7 +18,27 @@ import {RouterLink, RouterLinkActive} from '@angular/router';
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  @Input() role: string = "visitor";
 
-  @Input() mySpaceTitle = "Mon espace";
+  constructor(
+      private readonly localStorageService: LocalStorageService,
+      private readonly apiService: ApiService,
+      private readonly router: Router
+  ) {}
+
+  async ngOnInit(): Promise<void> {
+    const jwtToken: string|null = this.localStorageService.getJwtToken();
+
+    if (jwtToken !== null) {
+      const user: UserModel = await this.apiService.getUser(jwtToken);
+      this.role = user.role;
+    }
+  }
+
+  public async logout(): Promise<void> {
+    this.localStorageService.deleteJwtToken();
+
+    await this.router.navigate(['/home']);
+  }
 
 }
