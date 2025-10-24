036dadfe22d2122b7432717d0634c3a5e64d6059
Saltman0 <baudoin.mathieu@protonmail.com>
2025-10-24 16:10:27 +0200

    🔥 App config : Apollo Jwt token removed

--- src/components/app/app.config.ts
+++ src/components/app/app.config.ts
@@ -5,7 +5,6 @@ import { provideHttpClient, withFetch } from "@angular/common/http";
 import { provideApollo } from "apollo-angular";
 import { HttpLink } from "apollo-angular/http";
 import { InMemoryCache } from "@apollo/client/core";
-import { setContext } from "@apollo/client/link/context";
 import {environment} from "../../environments/environment";
 
 export const appConfig: ApplicationConfig = {
@@ -16,20 +15,6 @@ export const appConfig: ApplicationConfig = {
     ),
     provideApollo(() => {
       const httpLink = inject(HttpLink);
-
-      const auth = setContext(() => {
-        const token = localStorage.getItem("jwtToken");
-
-        if (token === null) {
-          return {};
-        } else {
-          return {
-            headers: {
-              Authorization: `Bearer ${token}`,
-            },
-          };
-        }
-      });
 
       return {
         link: httpLink.create({ uri: environment.GRAPHQL_API_URL + 'graphql' }),
