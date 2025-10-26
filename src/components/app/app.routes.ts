19593ea23e9a89e56ced9b403d48b8b11c2b98c4
Saltman0 <baudoin.mathieu@protonmail.com>
2025-10-26 15:51:20 +0100

    ✨ Movie route added

--- src/components/app/app.routes.ts
+++ src/components/app/app.routes.ts
@@ -1,6 +1,7 @@
 import {Routes} from '@angular/router';
 import {HomeComponent} from '../home/home.component';
 import {LoginComponent} from "../login/login.component";
+import {MoviesComponent} from "../movies/movies.component";
 import {OrderComponent} from '../order/order.component';
 import {EmployeeListComponent} from '../employee-list/employee-list.component';
 import {MovieShowtimeSettingsComponent} from '../movie-showtime-settings/movie-showtime-settings.component';
@@ -21,6 +22,7 @@ export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', title: "Home page", component: HomeComponent },
   { path: 'login', title: "Login page", component: LoginComponent },
+  { path: 'movies', title: "Movies page", component: MoviesComponent },
   { path: 'booking', title: "Booking page", component: BookingComponent },
   { path: 'order', title: "Order page", component: OrderComponent, canActivate: [UserGuard, AuthGuard] },
   { path: 'employee', title: "Employee page", component: EmployeeListComponent, canActivate: [AdminGuard, AuthGuard] },
