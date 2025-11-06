1b00be5698a3f581e81539a57f88680a21c80dc0
Saltman0 <baudoin.mathieu@protonmail.com>
2025-11-06 22:18:31 +0100

    ♻️ GetMovies GraphQL updated

--- src/app/core/graphql/get-movies.gql.ts
+++ src/app/core/graphql/get-movies.gql.ts
@@ -3,11 +3,13 @@ import {Injectable} from '@angular/core';
 import {MovieModel} from "../models/movie.model";
 import {RatingModel} from "../models/rating.model";
 import {ShowtimeModel} from "../models/showtime.model";
+import {HallModel} from "../models/hall.model";
 
 export interface Response {
     movies: MovieModel[];
     ratings: RatingModel[];
     showtimes: ShowtimeModel[];
+    hall: HallModel;
 }
 
 @Injectable({
@@ -15,8 +17,8 @@ export interface Response {
 })
 export class GetMoviesGql extends Query<Response> {
     document = gql`
-        query GetMovies {
-            movies {
+        query GetMovies($categoryId: Int, $startDate: String, $endDate: String, $cinemaId: Int) {
+            movies(categoryId: $categoryId) {
                 id
                 favorite
                 title
@@ -27,13 +29,15 @@ export class GetMoviesGql extends Query<Response> {
                     id
                     number
                 }
-                showtimes {
+                showtimes(startDate: $startDate, endDate: $endDate) {
                     id
                     startTime
                     endTime
+                    hall(cinemaId: $cinemaId) {
+                        id
+                    }
                 }
             }
         }
-
     `;
 }
